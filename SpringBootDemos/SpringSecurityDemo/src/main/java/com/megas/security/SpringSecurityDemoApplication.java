1c964d6006678ab1221a2d1a0a3ab2e3cc38e4f2
megasolutions2023 <151733291+megasolutions2023@users.noreply.github.com>
2024-08-18 12:44:21 +0530

    Spring Security: A Step-by-Step Guide to Secure Web Application
    Spring Security: A Step-by-Step Guide to Secure Web Application

---
+++ SpringBootDemos/SpringSecurityDemo/src/main/java/com/megas/security/SpringSecurityDemoApplication.java
@@ -0,0 +1,14 @@
+package com.megas.security;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SpringSecurityDemoApplication.class, args);
+	}
+
+}
---
+++ SpringBootDemos/SpringSecurityDemo/src/main/java/com/megas/security/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.megas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/", "/home").permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin((form) -> form
+				.loginPage("/login")
+				.permitAll()
+			)
+			.logout((logout) -> logout.permitAll());
+
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder() //Using this method is not considered safe for production, but is acceptable for demos and getting started. 
+				.username("admin")
+				.password("admin")
+				.roles("USER")
+				.build(); 
+		
+	
+
+		return new InMemoryUserDetailsManager(user);
+	}
+
+}
